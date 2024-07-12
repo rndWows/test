c1dc0da53cd014f3d7a9cf54c2fe73c2cc779676
rndWows <rnd8@wows.vn>
2024-07-12 15:55:10 +0700

    sd

--- app.js
+++ app.js
@@ -1,92 +1,77 @@
-var CLIENT_ID = '36866534114-quffe9ok3s05kab59mfuvt0s4a7eqqer.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyBIYrumhRpzcwz8E8FToH-dMoGfYFYQz90';
-var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+const CLIENT_ID = '36866534114-quffe9ok3s05kab59mfuvt0s4a7eqqer.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyBIYrumhRpzcwz8E8FToH-dMoGfYFYQz90';
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
 
-var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
 
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
-var content = document.getElementById('content');
+function gapiLoaded() {
+    gapi.load('client', intializeGapiClient);
+}
+
+async function intializeGapiClient() {
+    await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    maybeEnableButtons();
+}
 
 function handleClientLoad() {
-    gapi.load('client:auth2', initClient); // Tải và khởi tạo thư viện
+    gapi.load('client:auth2', initClient);
 }
 
 function initClient() {
     gapi.client.init({
         apiKey: API_KEY,
         clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
+        discoveryDocs: [DISCOVERY_DOC],
         scope: SCOPES
     }).then(function () {
         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
         updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
     }, function(error) {
-        console.error("Error loading GAPI client for API", error);
+        appendPre(JSON.stringify(error, null, 2));
     });
 }
-
 
 function updateSigninStatus(isSignedIn) {
     if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
         listMajors();
     } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
-        appendPre("Please log in to the application.");
+        console.log('Please log in to the application.');
     }
 }
 
-function handleAuthClick(event) {
+function handleAuthClick() {
     gapi.auth2.getAuthInstance().signIn();
 }
 
-function handleSignoutClick(event) {
+function handleSignoutClick() {
     gapi.auth2.getAuthInstance().signOut();
 }
 
-function listMajors() {
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: '1tzyxVXDZyk7FQtYm8nFYskisu5gdAf2QoV3SoDCzIRY',
-        range: 'PNK!A1:D5',
-    }).then(function(response) {
-        var range = response.result;
-        if (range.values.length > 0) {
-            appendPre('Name, Major:');
-            for (var i = 0; i < range.values.length; i++) {
-                var row = range.values[i];
-                appendPre(row[0] + ', ' + row[4]);
-            }
-        } else {
-            appendPre('No data found.');
-        }
-    }, function(response) {
-        appendPre('Error: ' + response.result.error.message);
-    });
-}
-
-function updateSheet() {
-    var values = [
-        ["Updated Name", "Updated Major"]
-    ];
-    var body = {
-        values: values
-    };
-    gapi.client.sheets.spreadsheets.values.update({
-         spreadsheetId: '1tzyxVXDZyk7FQtYm8nFYskisu5gdAf2QoV3SoDCzIRY',
-         range: 'PNK!A2',
-         valueInputOption: 'RAW',
-         resource: body
-    }).then(function(response) {
-        var result = response.result;
-        appendPre(`${result.updatedCells} cells updated.`);
-    });
-}
-
 function appendPre(message) {
-    content.innerHTML += message + '\n';
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+}
+
+async function listMajors() {
+    let response = await gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1tzyxVXDZyk7FQtYm8nFYskisu5gdAf2QoV3SoDCzIRY',
+        range: 'sheeq!A2:E',
+    });
+    const range = response.result;
+    if (range.values.length > 0) {
+        for (i = 0; i < range.values.length; i++) {
+            var row = range.values[i];
+            console.log(`Name: ${row[0]}, Major: ${row[4]}`);
+        }
+    } else {
+        console.log('No data found.');
+    }
 }
